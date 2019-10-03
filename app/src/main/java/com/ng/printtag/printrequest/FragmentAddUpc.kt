9bce4e6e2a99f9f5cd63e84dec577584c36bcbdb
kesha.purohit <kesha.purohit@credencys.com>
2019-10-03 18:22:58 +0530

    # added seekbar

---
+++ app/src/main/java/com/ng/printtag/printrequest/FragmentAddUpc.kt
@@ -0,0 +1,52 @@
+package com.ng.printtag.printrequest
+
+import android.widget.SeekBar
+import com.ng.printtag.R
+import com.ng.printtag.databinding.FragmentAddUpcBinding
+import ng.pdp.base.BaseFragment
+
+
+class FragmentAddUpc : BaseFragment<FragmentAddUpcBinding>() {
+    private lateinit var binding: FragmentAddUpcBinding
+    override fun initFragment() {
+        binding = getFragmentDataBinding()
+        init()
+        handleClick()
+    }
+
+    private fun init() {
+        binding.seekBar.progress = 1
+        binding.tvValue.text = binding.seekBar.progress.toString()
+
+        binding.seekBar.max = 24
+
+
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                binding.tvValue.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
+        })
+
+    }
+
+    private fun handleClick() {
+        binding.ivMinus.setOnClickListener {
+            binding.seekBar.progress = binding.seekBar.progress - 1
+        }
+        binding.ivPlus.setOnClickListener {
+            binding.seekBar.progress = binding.seekBar.progress + 1
+
+        }
+    }
+
+    override fun getLayoutId(): Int = R.layout.fragment_add_upc
+}
